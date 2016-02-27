05360beff62c1276b48bcad395941c06e7fb5dda
stevemario <steve.mario.correa@mail.com>
2016-02-27 14:15:24 -0800

    started 1st task of loading cities from a file

--- CSIT SMC S16 Assignment 2.cpp
+++ CSIT SMC S16 Assignment 2.cpp
@@ -5,5 +5,24 @@
 //Assignment 2
 //This software manages GenghisAir flights using graphs to represent the
 //available flights.
+#include <vector>
+class city;
+class connection {
+	public: city* destination;
+	public: int distance;
+};
+class city {
+	private: std::string sName;
+	private: std::vector <connection*> connections;
+};
+void loadVerticesEdgesAndWeights (std::vector<city>& cities);
 void main () {
+	std::vector<city> cities;
+	loadVerticesEdgesAndWeights (cities);
+}
+//Loads, as vertices, cities,
+//loads, as edges, connections, and
+//loads, as weights, distances in miles between two connected cities,
+//and saves all that information into my cities vector.
+void loadVerticesEdgesAndWeights (std::vector<city>& cities) {
 }
