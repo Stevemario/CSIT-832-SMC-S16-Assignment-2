81a1de4ccf3b034d6fe93f3128898756526d9ae2
stevemario <steve.mario.correa@mail.com>
2016-02-27 13:33:05 -0800

    added description comment and main function

---
+++ CSIT SMC S16 Assignment 2.cpp
@@ -0,0 +1,9 @@
+//Los Angeles Valley College
+//CSIT 832
+//Spring 2016
+//Steve Mario Correa
+//Assignment 2
+//This software manages GenghisAir flights using graphs to represent the
+//available flights.
+void main () {
+}
