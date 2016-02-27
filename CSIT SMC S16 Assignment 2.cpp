16d44bcb55826dcfc466d956d2f11fae2ddf2c17
stevemario <steve.mario.correa@mail.com>
2016-02-27 15:38:13 -0800

    added info within code (vs file, to save time)

--- CSIT SMC S16 Assignment 2.cpp
+++ CSIT SMC S16 Assignment 2.cpp
@@ -25,4 +25,11 @@ void main () {
 //loads, as weights, distances in miles between two connected cities,
 //and saves all that information into my cities vector.
 void loadVerticesEdgesAndWeights (std::vector<city>& cities) {
+	std::string sFilePlaceHolder =
+		"Los Angeles, San Diego, Phoenix, Las Vegas, San Fransisco\n"
+		"San Diego, Tucson, Phoenix, Los Angeles\n"
+		"Tucson, Phoenix, San Diego\n"
+		"Phoenix, Tucson, Las Vegas, Los Angeles, San Diego\n"
+		"Las Vegas, Phoenix, San Francisco, Los Angeles\n"
+		"San Francisco, Los Angeles, Las Vegas\n";
 }
